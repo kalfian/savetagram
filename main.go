f0d329ba85ece22b499d56eb66276eb930a18791
Kukuh <hi@kalfian.com>
2021-07-26 23:29:02 +0700

    DEBUG Webhook

--- main.go
+++ main.go
@@ -2,7 +2,10 @@ package main
 
 import (
 	"log"
+	"net/http"
+	"os"
 
+	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api"
 	"github.com/joho/godotenv"
 )
 
@@ -11,4 +14,37 @@ func main() {
 	if err != nil {
 		log.Fatal("Error loading .env file")
 	}
+
+	botToken := os.Getenv("BOT_TOKEN")
+
+	bot, err := tgbotapi.NewBotAPI(botToken)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	bot.Debug = true
+
+	log.Printf("Authorized on account %s", bot.Self.UserName)
+
+	botUrl := os.Getenv("BOT_URL")
+
+	_, err = bot.SetWebhook(tgbotapi.NewWebhook(botUrl + bot.Token))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	info, err := bot.GetWebhookInfo()
+	if err != nil {
+		log.Fatal(err)
+	}
+	if info.LastErrorDate != 0 {
+		log.Printf("Telegram callback failed: %s", info.LastErrorMessage)
+	}
+	updates := bot.ListenForWebhook("/" + bot.Token)
+	go http.ListenAndServe(":"+os.Getenv("PORT"), nil)
+
+	for update := range updates {
+		log.Printf("%+v\n", update)
+	}
+
 }
