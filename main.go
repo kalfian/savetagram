a42b7e461b5c2f1d81191a12e1de2b3091d74cdc
Kukuh <hi@kalfian.com>
2021-07-26 23:57:15 +0700

    Test Bot

--- main.go
+++ main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"log"
-	"net/http"
 	"os"
 
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api"
@@ -19,32 +18,29 @@ func main() {
 
 	bot, err := tgbotapi.NewBotAPI(botToken)
 	if err != nil {
-		log.Fatal(err)
+		log.Panic(err)
 	}
 
 	bot.Debug = true
 
 	log.Printf("Authorized on account %s", bot.Self.UserName)
 
-	botUrl := os.Getenv("BOT_URL")
+	u := tgbotapi.NewUpdate(0)
+	u.Timeout = 60
 
-	_, err = bot.SetWebhook(tgbotapi.NewWebhook(botUrl + bot.Token))
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	info, err := bot.GetWebhookInfo()
-	if err != nil {
-		log.Fatal(err)
-	}
-	if info.LastErrorDate != 0 {
-		log.Printf("Telegram callback failed: %s", info.LastErrorMessage)
-	}
-	updates := bot.ListenForWebhook("/" + bot.Token)
-	go http.ListenAndServe(":"+os.Getenv("PORT"), nil)
-
+	updates, err := bot.GetUpdatesChan(u)
+	// go http.ListenAndServe(":"+os.Getenv("PORT"), nil)
 	for update := range updates {
-		log.Printf("%+v\n", update)
+		if update.Message == nil { // ignore any non-Message Updates
+			continue
+		}
+
+		log.Printf("[%s] %s", update.Message.From.UserName, update.Message.Text)
+
+		msg := tgbotapi.NewMessage(update.Message.Chat.ID, update.Message.Text)
+		msg.ReplyToMessageID = update.Message.MessageID
+
+		bot.Send(msg)
 	}
 
 }
